3543823ec305921c2e5d83acde3b197181f7b4ce
vinothjayaraman <vinothjayaram@outlook.com>
2017-12-04 23:53:16 -0500

    completing the vertical menu & small device responsive menu

--- src/fw/services/menu.service.ts
+++ src/fw/services/menu.service.ts
@@ -10,5 +10,16 @@ export interface MenuItem{
 @Injectable()
 export class MenuService{
     items: Array<MenuItem>;
-    isVertical: boolean = true;
+    isVertical: boolean = false;
+    showingLeftSideMenu: boolean = false;
+
+    toggleLeftSideMenu() : void {
+        this.isVertical = true;
+        this.showingLeftSideMenu = !this.showingLeftSideMenu;
+    }
+
+    closeLeftSideMenu() : void {
+        this.isVertical = false;
+        this.showingLeftSideMenu = false;
+    }
 }
